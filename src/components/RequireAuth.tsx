758be6f5927abbdeea6a6c621e6d4c64097c5cfe
okashehserag-dotcom <okashehserag@gmail.com>
2026-02-09 16:42:58 +0300

    Update RequireAuth.tsx

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,1 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../state/auth";
+
+export function RequireAuth({ children }: { children: JSX.Element }) {
+  const { user, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to="/login" />;
+
+  return children;
+}
 
