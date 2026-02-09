57fdc818680b28ba4ec790469a507a2199e75d7f
okashehserag-dotcom <okashehserag@gmail.com>
2026-02-09 16:39:15 +0300

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,1 +1,26 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { RequireAuth } from "../components/RequireAuth";
+import { DashboardLayout } from "../layouts/DashboardLayout";
+
+import { Login } from "../pages/Login";
+import { TimerPage } from "../pages/Timer";
+import { DashboardHome } from "../pages/DashboardHome";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <Navigate to="/app/timer" /> },
+  { path: "/login", element: <Login /> },
+
+  {
+    path: "/app",
+    element: (
+      <RequireAuth>
+        <DashboardLayout />
+      </RequireAuth>
+    ),
+    children: [
+      { index: true, element: <DashboardHome /> },
+      { path: "timer", element: <TimerPage /> },
+    ],
+  },
+]);
 
